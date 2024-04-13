c2d7f75a7ae9abc2cfd763a9655eea5748a4e17b
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-04-12 21:22:25 -0300

    primeiro commit

---
+++ aula001_primeira_programa/aula001_hello_word.py
@@ -0,0 +1,19 @@
+# Curso de Desenvolvimento de Sistemas
+# Autor: Vinícius Estevam da Silva
+# Data: 12/04/2024
+# Primeiro Programa em Python
+
+
+# Importando as bibliotecas
+import os
+
+
+# Limpando o terminal
+os.system('cls')
+
+# O comando print() executa uma saida via terminal
+
+print('-' * 70)
+print('Hello Word!!!')
+print('-' * 70)
+print()
