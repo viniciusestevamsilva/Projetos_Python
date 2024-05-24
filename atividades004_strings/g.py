0fc212cb8c640fcf09630441572d43e506385801
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-05-23 21:37:08 -0300

    Atividade004 Strings

--- atividades004_strings/g.py
+++ atividades004_strings/g.py
@@ -13,16 +13,19 @@ print('-'*70)
 
 numero = int(input('Insira um numero: '))
 
-# PROCESSAMENTO
-unidades = numero % 10
-dezenas = (numero // 10) % 10
-centenas = (numero // 100) % 10
-milhares = (numero // 1000) % 10
+#VALIDAÇÃO
 
-# SAIDA
-print(f'Em unidades:{unidades}')
-print(f'Em dezenas:{dezenas}')
-print(f'Em centenas:{centenas}')
-print(f'Em milhares:{milhares}')
-print('-'*70)
-print()
+if numero < 0 :
+    print('Coloque um numero valido!!!')
+else:
+    unidades = numero % 10
+    dezenas = (numero // 10) % 10
+    centenas = (numero // 100) % 10
+    milhares = (numero // 1000) % 10
+    
+    print(f'Em unidades:{unidades}')
+    print(f'Em dezenas:{dezenas}')
+    print(f'Em centenas:{centenas}')
+    print(f'Em milhares:{milhares}')
+    print('-'*70)
+    print()
