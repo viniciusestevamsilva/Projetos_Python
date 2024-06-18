62b03599c359628c6a81b4a705f4a788b3718d8b
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-06-17 21:35:47 -0300

    Atividade

--- atividades/atividades006_listas/c.py
+++ atividades/atividades006_listas/c.py
@@ -20,59 +20,67 @@ print('''
 ''')
 print('-'*70)
 
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+lista_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+produtos = []
 
-#Os números ímpares
-numeros_impares = 0 
-#Os números pares
-numero_pares = 0
-#Os múltiplos de 2, 3 e 4
-multiplos_de_2 = 0
-multiplos_de_3 = 0
-multiplos_de_4 = 0
+#Os números ímpares e pares
+numeros_impar = 0 
+numero_par = 0
+
+#Os múltiplos
+multiplo_de_2 = 0
+multiplo_de_3 = 0
+multiplo_de_4 = 0
 #Lista reversa
-lista_reversa = lista
+lista_reversa = lista_numeros
 
 
 #O intervalo de 1 até 9 e 8 até 13
-intervalo_1_9 = lista[0:9]
-intervalo_8_13 = lista[8:13]
+intervalo_1_9 = lista_numeros[0:9]
+intervalo_8_13 = lista_numeros[8:13]
 
 print('Numeros impares: ') # Os números ímpares
-while numeros_impares <= 15:
-    numeros_impares += 1
-    if (numeros_impares % 2 == 0):
+while numeros_impar <= 15:
+    numeros_impar += 1
+    if (numeros_impar % 2 == 0):
         continue
-    print(f'{numeros_impares}')
+    print(f'{numeros_impar}')
 else:
-    numeros_impares -= 1
+    numeros_impar -= 1
     print('')
     print('.'*70)
     
 print('Numeros pares: ')#Os números pares
-while numero_pares <= 15:
-    numero_pares += 1
-    if numero_pares % 2 != 0: #pulando os impares
+while numero_par <= 15:
+    numero_par += 1
+    if numero_par % 2 != 0: #pulando os impares
         continue
-    print(f'{numero_pares}')
+    print(f'{numero_par}')
     
 else:
-    numero_pares -= 1
+    numero_par -= 1
     print('')
     print('.'*70)
 
 # Verificando múltiplos de 2, 3 e 4
-multiplos_de_2 = [num for num in lista if num % 2 == 0]
-multiplos_de_3 = [num for num in lista if num % 3 == 0]
-multiplos_de_4 = [num for num in lista if num % 4 == 0]
+multiplo_de_2 = [num for num in lista_numeros if num % 2 == 0]
+multiplo_de_3 = [num for num in lista_numeros if num % 3 == 0]
+multiplo_de_4 = [num for num in lista_numeros if num % 4 == 0]
 
 # saida
-print(f'Múltiplos de 2:{multiplos_de_2}')
-print(f'Múltiplos de 3:{multiplos_de_3}')
-print(f'Múltiplos de 4:{multiplos_de_4}')
+print(f'Múltiplos de 2:{multiplo_de_2}')
+print(f'Múltiplos de 3:{multiplo_de_3}')
+print(f'Múltiplos de 4:{multiplo_de_4}')
 print('.'*70)
 
 #Revertendo a lista
+lista_reversa = lista_numeros.copy()
 lista_reversa.reverse()
 print(f'Lista revertida: {lista_reversa}')
 print('.'*70)
+
+for numero_a in lista_numeros[4:6]:
+    for numero_b in lista_numeros[10:12]:
+        produto = numero_a + numero_b
+        produtos.append(produto)
+print(produto)
