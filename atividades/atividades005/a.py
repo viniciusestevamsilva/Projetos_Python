82e45799aa11cee5dbb0e726acb6fd8709f5eac3
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-06-18 19:31:04 -0300

    Atividades

--- atividades/atividades005/a.py
+++ atividades/atividades005/a.py
@@ -11,9 +11,12 @@ print('-'*70)
 print('Tudo na mesma linha em horizontal')
 print('-'*70)
 
+print('Números:')
 for valores in range (1, 100):
     # end= coloca os números em uma mesma linha
-    print(f'Números: {valores}', end=" || ")
-    
+    print(valores , end="  ")
+print()
+print()
+
 print('-'*70)
 print()
