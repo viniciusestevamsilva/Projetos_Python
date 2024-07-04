d078802c445ba490f034b7f8eab22ffae949f788
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-07-04 20:59:55 -0300

    palmeiras

--- atividades/atividades009_dicionarios/d.py
+++ atividades/atividades009_dicionarios/d.py
@@ -16,20 +16,19 @@ print('='*70)
 print('Programa que cria um dicionário com 5 cores.')
 print('-'*70)
 
-# Declaração de dicionario
-dicionario = dict()
-cores = []
+dicionario = dict() # Dicionario
+cores = [] # Lista
 
 for i in range(5):
     print(f'Informe os dados da {i + 1}º cor:')
     nome = str(input('Nome da cor: '))
     classificacao = str(input('Cor primaria,segundario ou terciaria?: '))
-    dicionario[nome] = classificacao
+    dicionario[nome] = classificacao # Colocando valor no dicionario
     print('-'*70)
     cores.append(dicionario.copy())# Adicionando uma copia a lista
 
-print('Cores Adicionadas:')
+print('Cores Adicionadas')
 for cores in dicionario:
-    for chave,valor in dicionario.items():
+    for chave,valor in dicionario.items(): # Para cada chave e o valor do dicionario
         print('-'*70)
         print(f'{chave}: {valor}')
