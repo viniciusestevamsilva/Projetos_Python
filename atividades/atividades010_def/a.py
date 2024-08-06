59c1a2c85fa7930b17de8e09099708e0dbe6d53d
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-08-06 20:19:51 -0300

    a

--- atividades/atividades010_def/a.py
+++ atividades/atividades010_def/a.py
@@ -4,20 +4,18 @@
 
 # Crie uma função que receba uma lista de números. Depois retorne duas listas com os números pares,
 # os números ímpares, a quantidade de números pares e a quantidade de números ímpares.
-# Faça um programa para criar um dicionário com 4 elementos.
+
 
 import os
-
+import random
 
 os.system('cls')
 
 
-lista = [0,1,2,3,4,5,6,7,8,9]
-# pares = [0, 2, 4, 6, 8]
-# impares = [1, 3, 5, 7, 9]
+lista = [random.randint(0, 10) for i in range(10)]
 
 
-def par_impar(lista):
+def numeros(lista):
     """separando par e impar
 
     Args:
@@ -28,6 +26,7 @@ def par_impar(lista):
     """    
     par = []
     impar = []
+    
     for num in lista:
         if num % 2 == 0:
             pares = num
@@ -37,10 +36,10 @@ def par_impar(lista):
             impares = num
             impar.append(impares)
             quantidade_i = len(impar)
-    print(f'Pares : {par}')
-    print(f'Quantidade de pares: {quantidade_p}')
-    print(f'Impares: {impar}')
-    print(f'Quantidade de impares: {quantidade_i}')
+    par.sort()
+    impar.sort()
+    print(f'Pares : {par}\nQuantidade de números pares: {quantidade_p}')
+    print(f'Impares: {impar}\nQuantidade de números impares: {quantidade_i}')
     return lista
 
-par_impar(lista)
+numeros(lista)
