33dec1e1f816648f4ee8a65a541d4a0735c186c9
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-09-04 20:34:48 -0300

    a

--- atividades/atividades011_poo/a.py
+++ atividades/atividades011_poo/a.py
@@ -29,53 +29,78 @@ while True:
     print('='*70)
 
     a = input('Digite o 1° valor: ')
+    
+    if a.isalpha() or a == '':
+        os.system('cls')
+        print('-'*70)
+        print('Digite apenas números')
+        input('Digite enter pra voltar')
+        continue
+    
     b = input('Digite o 2° valor: ')
+    
+    if b.isalpha() or b == '':
+        os.system('cls')
+        print('-'*70)
+        print('Digite apenas números')
+        input('Digite enter pra voltar')
+        continue
+    
     c = input('Digite o 3° valor: ')
     
+    if c.isalpha() or c == '':
+        os.system('cls')
+        print('-'*70)
+        print('Digite apenas números')
+        input('Digite enter pra voltar')
+        continue
+    
+    a = float(a)
+    b = float(b)
+    c = float(c)
     
     resultado = Conta(a, b, c)  # Criando um objeto
-
-    opcao = str(input('Você deseja Somar ou multiplicar? ')).lower()
-    if opcao == 'somar':
-            
-        soma = resultado.soma(a,b,c)
-            
+    adicao = resultado.soma(a, b, c)
+    vezes = resultado.multiplicar(a, b, c)
+    
+    opcao = str(input('Você deseja Somar ou multiplicar (s/m)?\n(caso queira sair digite, "n") ')).lower()
+    
+    if opcao == 'n':
+        
         os.system('cls')
-
-        print()
-        print('-'*70)
-        print('Resultados da conta:')
-        print(f'A soma é: {soma}')
-        print('/'*70)
-        input('Digite enter pra voltar')
-            
-    elif opcao == 'multiplicar':
-        multiplicar = resultado.multiplicar(a,b,c)
-            
-        os.system('cls')
-            
-        print('/'*70)
-        print()
-        print('-'*70)
-        print('Resultados da conta:')
-        print(f'A multiplicação é: {multiplicar}')
-        print('/'*70)
-        input('Digite enter pra voltar')
-    else:
-
-        print('Opção inváida')
-        input('Digite enter pra voltar')
-            
-    os.system('cls')
-    print('/'*70)
-    continuar = input('Deseja continuar ou sair do programa (s/n)? ').lower()
-    print('='*70)
-    if continuar == 'n':
-        os.system('cls')
+        
         print('Programa Finalizado')
         print('/'*70)
         break
-    else:
-
-        print('Opção inváida')
+    
+    elif opcao.isnumeric() or opcao == '':
+        
+        print('-'*70)
+        print('Digite algo valido')
         input('Digite enter pra voltar')
+    else: 
+        if opcao == 's':
+        
+            os.system('cls')
+
+            print()
+            print('-'*70)
+            print('Resultados da conta:')
+            print(f'A soma é: {adicao}')
+            input('Digite enter pra continuar')
+                
+        elif opcao == 'm':
+                
+            os.system('cls')
+                
+            print('/'*70)
+            print()
+            print('-'*70)
+            print('Resultados da conta:')
+            print(f'A multiplicação é: {vezes}')
+            print('/'*70)
+            input('Digite enter pra continuar')
+        else:
+            print('/'*70)
+            print('Opção inváida')
+            input('Digite enter pra voltar')
