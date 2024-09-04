241303286ad98e826a4859cb8fca1efa5f65c0a2
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-09-04 19:46:38 -0300

    a

--- atividades/atividades011_poo/a.py
+++ atividades/atividades011_poo/a.py
@@ -1,39 +1,81 @@
 # Curso de Desenvolvimento de sistemas
 # Autor : Vinícius Estevam da Silva
 # Data 03/09/24
-import os
+import os  # Importanto bibliotecas
 
 
-class Conta():
-    def __init__(self, a, b, c):
+class Conta():  # Criando a classe
+    def __init__(self, a, b, c):  # Metodo construtor
         self.a = a
         self.b = b
         self.c = c
-        
-    def soma(self, a, b, c):
+
+    # Metodos
+    def soma(self, a, b, c):  # Função para somar
         soma = a + b + c
         return soma
-        
-    def multiplicar(self, a, b, c):
-        
+
+    def multiplicar(self, a, b, c):  # Função para multiplicar
+
         multiplicar = a * b * c
         return multiplicar
-        
+
+
 while True:
-    
+
     os.system('cls')
-    print('-'*70)
+    print('/'*70)
     print('Multipicar e somar três valores')
     print('='*70)
-    a = float(input('Digite o 1° valor: '))
-    b = float(input('Digite o 2° valor: '))
-    c = float(input('Digite o 3° valor: '))
-    
-    resultado = Conta(a,b,c)
 
-    print()
-    print('-'*70)
-    print('Resultados da conta:')
-    print(f'A soma é: {resultado.soma(a,b,c)}')
-    print(f'A multiplicação é: {resultado.multiplicar(a,b,c)}')
+    a = input('Digite o 1° valor: ')
+    b = input('Digite o 2° valor: ')
+    c = input('Digite o 3° valor: ')
+    
+    
+    resultado = Conta(a, b, c)  # Criando um objeto
+
+    opcao = str(input('Você deseja Somar ou multiplicar? ')).lower()
+    if opcao == 'somar':
+            
+        soma = resultado.soma(a,b,c)
+            
+        os.system('cls')
+
+        print()
+        print('-'*70)
+        print('Resultados da conta:')
+        print(f'A soma é: {soma}')
+        print('/'*70)
+        input('Digite enter pra voltar')
+            
+    elif opcao == 'multiplicar':
+        multiplicar = resultado.multiplicar(a,b,c)
+            
+        os.system('cls')
+            
+        print('/'*70)
+        print()
+        print('-'*70)
+        print('Resultados da conta:')
+        print(f'A multiplicação é: {multiplicar}')
+        print('/'*70)
+        input('Digite enter pra voltar')
+    else:
+
+        print('Opção inváida')
+        input('Digite enter pra voltar')
+            
+    os.system('cls')
+    print('/'*70)
+    continuar = input('Deseja continuar ou sair do programa (s/n)? ').lower()
     print('='*70)
+    if continuar == 'n':
+        os.system('cls')
+        print('Programa Finalizado')
+        print('/'*70)
+        break
+    else:
+
+        print('Opção inváida')
+        input('Digite enter pra voltar')
