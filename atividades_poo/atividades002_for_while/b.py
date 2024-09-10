d988b6a95c6ae277a38c577dca0becf63ff92192
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-09-10 20:59:49 -0300

    B - Finalizada

--- atividades_poo/atividades002_for_while/b.py
+++ atividades_poo/atividades002_for_while/b.py
@@ -6,32 +6,37 @@ import os
 
 class Intervalo:
     def __init__(self, comeco, fim):
-        self.comeo = comeco
+        # Método construtor
+        self.comeco = comeco  # Atributos
         self.fim = fim
-
 
 class Gerador(Intervalo):
     def __init__(self, comeco, fim):
+        # Construtor da classe que inicializa os atributos
         self.comeco = comeco
         self.fim = fim
-        
-    def de1a100(self, comeco, fim):
 
-        inicio = comeco
-        while inicio <= fim:
-            print(f'numero: {inicio}', end=' | ')
+    def de1a100(self):
+        inicio = self.comeco
+        while inicio <= self.fim:
+            print(f'Número: {inicio}', end='  ')
             inicio += 1
+        print()
 
 
 os.system('cls')
-print('/'*70)
-print('Gerar numeros, digite o inicio eo fim do ciclo')
-print('='*70)
-inicio = int(input('Digite o valor inicial: '))
-final = int(input('Digite o valor final: '))
-print('')
-gerador = Gerador(inicio, final)
-resultado = gerador.de1a100(inicio, final)
-print('')
-print('')
-print('/'*70)
+
+print('/' * 70)
+print('Imprimir números em um intervalo definido pelo usuário')
+print('=' * 70)
+
+comeco = int(input('Digite o início do intervalo: '))
+fim = int(input('Digite o fim do intervalo: '))
+
+# Cria uma instância da classe
+gerador = Gerador(comeco, fim)
+
+# Chama o método
+gerador.de1a100()
+
+print('/' * 70)
