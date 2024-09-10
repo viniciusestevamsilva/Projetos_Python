2af6247e03550b508bc2de257369f943316bbfc5
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-09-10 20:53:15 -0300

    A - Finalizada

--- atividades_poo/atividades002_for_while/a.py
+++ atividades_poo/atividades002_for_while/a.py
@@ -5,31 +5,33 @@ import os
 
 
 class Intervalo:
-    def __init__(self, comeco, fim):
-        self.comeco = comeco
-        self.fim = fim
-
-         
+    def __init__(self, comeco, fim):# Método construtor
+        self.comeco = comeco  # Atributos
+        self.fim = fim        
 
 class Gerador(Intervalo):
     def __init__(self, comeco, fim):
-        self.comeco = comeco
-        self.fim = fim
-        
-    def de1a100(self, comeco, fim):
+        # Construtor da classe que inicializa os atributos
+        self.comeco = comeco  
+        self.fim = fim        
 
-        inicio = comeco
-        while inicio <= fim:
-            print(f'numero: {inicio}', end='  ')
-            inicio += 1
-   
+    def de1a100(self):
+        inicio = self.comeco 
+        while inicio <= self.fim:
+            print(f'Número: {inicio}', end='  ')
+            inicio += 1 
+        print()
+
 
 os.system('cls')
-print('/'*70)
-print('Imprimir numeros de 1 a 100')
-print('='*70)
 
+print('/' * 70)
+print('Imprimir números de 1 a 100')
+print('=' * 70)
+
+# Cria uma instância da classe 
 gerador = Gerador(1, 100)
-resultado = gerador.de1a100(1, 100)
-print('')
-print('/'*70)
+# Chama o método 
+gerador.de1a100()
+
+print('/' * 70)
