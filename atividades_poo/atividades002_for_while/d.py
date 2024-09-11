da1e1e7edc5e065ccf7b596bd47525ff426447f3
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-09-10 21:08:05 -0300

    D - Finalizada

--- atividades_poo/atividades002_for_while/d.py
+++ atividades_poo/atividades002_for_while/d.py
@@ -6,22 +6,20 @@ import os
 
 class Pares:
     def __init__(self):
-        # Inicializa com valores padrão
-        self.numero = 0
+        # Método construtor
+        self.numero = 0 # Atributos
         self.numero_final = 100
 
     def processa(self):
-        # Método base que pode ser sobrescrito
         pass
 
 class Num_pares(Pares):
     def __init__(self, numero_inicial, numero_final):
-        # Inicializa com valores padrão e configura os atributos
+        # Construtor da classe que inicializa os atributos
         self.numero = numero_inicial
         self.numero_final = numero_final
 
     def conta(self):
-        # Contar e imprimir números pares entre numero e numero_final
         while self.numero <= self.numero_final:
             if self.numero % 2 == 0:
                 print(f'Número par: {self.numero}')
@@ -34,11 +32,9 @@ print('/' * 70)
 print('Imprimindo Números somente pares entre 0 e 100')
 print('=' * 70) 
 
-# Cria uma instância da classe Num_pares
-par = Num_pares(0, 100)  # Inicializa com o número inicial e final
+# Cria uma instância da classe 
+par = Num_pares(0, 100) 
 
-# Conta e imprime os números pares
-par.conta()
+par.conta()# Chama o método
 
 print('/' * 70)
-
