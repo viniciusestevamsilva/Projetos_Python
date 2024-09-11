3ceeb96fe87ea27cd8000a2a64cf90170f59c196
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-09-10 21:33:19 -0300

    incompleta

--- atividades_poo/atividades002_for_while/g.py
+++ atividades_poo/atividades002_for_while/g.py
@@ -6,35 +6,33 @@ import os
 
 
 class Numeros:
-    def __init__(self, inicio, final):
-        self.inicio = inicio
+    def __init__(self, inicio, final):  # Método construtor
+        self.inicio = inicio # Atributos
         self.final = final
         
-    def contador(self, contador):
-        self.contador = contador
-        
-        contador = 0
-        pass
-        
 class Primos(Numeros):
-    def par(self,inicio, final):
-        
-        inicio = 2
-        fim = final
-        self.contador = inicio
-        
-        for numero_primo in range(self.contador, fim):
-            for divisor in range(2, int(numero_primo**0.5) + 1):
-                if numero_primo % divisor == 0:
-                    break
-            else:
-                print(f'{numero_primo}', end=" | ")
+    def calcular_primos(self):
+        primos = []
+        for numero_primo in range(self.inicio, self.final + 1):
+            if numero_primo > 1:
+                for divisor in range(2, int(numero_primo**0.5) + 1):
+                    if numero_primo % divisor == 0:
+                        break
+                else:
+                    primos.append(numero_primo)
+        return primos
 
 os.system('cls')
+
 print('/'*70)
 inicio = int(input('Digite o valor inicial: '))
 final = int(input('Digite o valor final: '))
 print('='*70)
 
-resultados = Primos(inicio,final)
-impar = resultados.par(inicio,final)
+
+resultados = Primos(inicio, final) # Cria uma instância da classe 
+primos = resultados.calcular_primos() # Chama o método
+
+print(f'Números primos entre {inicio} e {final}:')
+for primo in primos:
+    print(primo)
