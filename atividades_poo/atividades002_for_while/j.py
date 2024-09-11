2ac6a460a11c58c7c6d83ae11c00c4525f24fd8a
viniciusestevamsilva <viniciusestevamsilva@gmail.com>
2024-09-11 19:34:57 -0300

    J - Finalizada

--- atividades_poo/atividades002_for_while/j.py
+++ atividades_poo/atividades002_for_while/j.py
@@ -2,11 +2,11 @@
 # Autor : Vinícius Estevam da Silva
 # Data 09/09/2024
 import os
-  # Importando o módulo os
 
-class Numeros:
-    def __init__(self, comeco):
-        self.comeco = comeco  # O parâmetro comeco já deve ser um inteiro
+
+class Numeros: # Classe pai
+    def __init__(self, comeco): # Metodo construtor
+        self.comeco = comeco  # Atributos
         
     def enunciado(self):
         print('/' * 70)
@@ -16,36 +16,33 @@ class Numeros:
     def contador(self):
         pass
 
-class Impar(Numeros):
+class Impar(Numeros): # Classe filha
     def __init__(self, comeco):
-        Numeros.__init__(self, comeco)  # Inicializando a classe base diretamente
+        Numeros.__init__(self, comeco)  # Iniciando a classe diretamente
 
     def contador(self):
         soma = 0
         quantidade = 0
         
-        for i in range(self.comeco, 101):  # Mudança para 101 para incluir 100, se necessário
+        for i in range(self.comeco, 101): 
             if i % 2 != 0:
                 print(f'{i}', end='   ')
                 soma += i
                 quantidade += 1
         return soma, quantidade
 
-# Limpa a tela
+
 os.system('cls')
 
-# Exibe o enunciado
 numeros = Numeros(0)
 numeros.enunciado()
 
-# Obtém o valor inicial do intervalo
+
 comeco = 1
 
-# Cria uma instância da classe Impar
-impar = Impar(comeco)
 
-# Calcula e imprime a soma e a quantidade de números ímpares
-soma, quantidade = impar.contador()
+impar = Impar(comeco)  # Cria uma instância da classe 
+soma, quantidade = impar.contador()  # Chama o método
 print()
 print(f'\nSoma dos números ímpares: {soma}')
 print(f'Quantidade de números ímpares: {quantidade}')
